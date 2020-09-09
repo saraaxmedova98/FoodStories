4a0daed34b1acd775ecb1e8690e4aeb9f8572d46
saraaxmedova98 <sariyataxmedova@gmail.com>
2020-09-10 02:01:28 +0400

    updated

--- foodStories/stories/templatetags/custom-tags.py
+++ foodStories/stories/templatetags/custom-tags.py
@@ -1,7 +1,7 @@
 from django import template
 from stories.forms import SubscribeForm
-from stories.models import SiteSettings
-
+from stories.models import *
+import datetime
 register = template.Library()
 
 @register.simple_tag
@@ -12,3 +12,39 @@ def subscribe_form():
 @register.simple_tag
 def site_settings():
     return SiteSettings.objects.last()
+
+@register.simple_tag()
+def stories_count():
+    
+    return Story.objects.all().count()
+
+@register.simple_tag()
+def recipes_count():
+
+    return Recipe.objects.all().count()
+
+@register.simple_tag()
+def recipe_count():
+    created_list = []
+    for recipe in Recipe.objects.all():
+        created_list.append(recipe.recipe_count)
+    return created_list
+
+@register.simple_tag()
+def story_count():
+    created_list = []
+    for story in Story.objects.order_by('-story_count')[:4]:
+        created_list.append(story.story_count)
+    return created_list
+
+
+
+@register.simple_tag()
+def story_title():
+    
+    return Story.objects.all()
+
+@register.simple_tag()
+def recipe_title():
+    
+    return Recipe.objects.all()
